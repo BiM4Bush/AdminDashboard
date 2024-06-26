a2b08852e1ff53feb8cf747ff7c984fa30f52ab6
BiM4Bush <sebakrzynek@gmail.com>
2024-06-26 13:14:01 +0200

    Created User Dtos

---
+++ AdminDashboard/AdminDashboard/Core/Dtos/Auth/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Core.Dtos.Auth
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+    }
+}
