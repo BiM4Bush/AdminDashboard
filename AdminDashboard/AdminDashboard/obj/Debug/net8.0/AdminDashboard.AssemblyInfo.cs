c9dce3de1547c668f6c7fa8360c5da732f977c24
BiM4Bush <sebakrzynek@gmail.com>
2024-06-27 14:17:06 +0200

    init fronted react-ts project

--- AdminDashboard/AdminDashboard/obj/Debug/net8.0/AdminDashboard.AssemblyInfo.cs
+++ AdminDashboard/AdminDashboard/obj/Debug/net8.0/AdminDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f82d3c0652050388030a7ba3a35de987d87d61e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f086178a8e4a5d482d91da8a177bd3f793e9b84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
